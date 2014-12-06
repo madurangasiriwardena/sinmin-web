8f89f9dfb450a44f07a0e0414c832c2fb9fc1ba4
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2014-12-06 14:56:37 +0530

    converter added to popularwords.php

--- popularwords.php
+++ popularwords.php
@@ -37,6 +37,10 @@
                                                 <label class="sinmin-label">Word</label>
                                                 <input class="sinmin-form-control" id="word">
                                             </div>
+                                            <div class="sinmin-form-group">
+                                                <span class="pull-right"><input type="button" class="btn btn-outline btn-primary" value="Type in Singlish" onclick="type_in_singlish('word')"></span>
+                                                
+                                            </div>
                                             <div class="sinmin-form-group" style="margin-top:20px">
                                                 <label  class="sinmin-label"><input type="checkbox" checked="true" id="enable-time" ></label>
                                                 <label  class="sinmin-label">Time</label>
@@ -90,6 +94,25 @@
         <!-- /#page-wrapper -->
 
     </div>
+
+    <div id="light_box" class="col-lg-6">
+        <form role="form" name="converter" id="converter" onsubmit="return false;">
+            <div class="sinmin-form-group">
+                <label class="sinmin-label">Singlish</label>
+                <textarea id="box1" onkeyup="startText();" onselect="startText();" onclick="startText();" style="font-size: 12pt; width: 350px;" name="box1" rows="3"></textarea>
+            </div>
+            <div class="sinmin-form-group" style="margin-top:20px">
+                <label class="sinmin-label">Unicode</label>
+                <textarea style="font-size: 14pt; font-family: Potha, Malithi Web , Arial Unicode MS; width: 350px;" name="box2" rows="3" id="box2"></textarea>
+            </div>
+            <br/>
+            <input type="hidden" id="input_id">
+            <button input="button" onclick="copyit()" class="btn btn-outline btn-primary">Ok</button>
+            <button type="reset" class="btn btn-outline btn-primary">Reset</button>
+        </form>
+        <a id="close_x" class="fa fa-close fa-fw close" href="#"></a>
+    </div>
+
     <!-- /#wrapper -->
 
     <!-- jQuery -->
@@ -112,13 +135,27 @@
     <script src="js/plugins/flot/jquery.flot.time.js"></script>
     <script src="js/plugins/flot/jquery.flot.categories.js"></script>
 
+    <script src="js/jquery.lightbox.js"></script>
+    <script src="js/converter.js"></script>
+
     <script type="text/javascript">
+
+    function type_in_singlish(input_id) {
+        $("#light_box").lightbox_me({centered: true, preventScroll: true, onLoad: function() {
+            $("#light_box").find("textarea:first").focus();
+        }});
+        $('#input_id').val(input_id);
+    };
+
+    function copyit () {
+        var text = $('#box2').val();
+        $("#"+$('#input_id').val()+"").val(text);
+        $("#light_box").trigger('close');
+    }
+
     $(document).ready(function(){
         document.getElementById("graph-panel").style.display = "none";
     });
-    </script>
-
-    <script type="text/javascript">
         $('#enable-time').change(function(){
             if($('#enable-time').is(':checked')){
                 document.getElementById("from").disabled = false;
@@ -129,27 +166,22 @@
             }
         });
         
-    </script>
-
-    <script type="text/javascript">
-        $('#enable-category').change(function(){
-            if($('#enable-category').is(':checked')){
-                var categories = document.getElementsByClassName("checkbox-category");
-                for (i = 0; i < categories.length; i++) {
-                    categories[i].disabled=false ;
-                }
-            }else{
-                var categories = document.getElementsByClassName("checkbox-category");
-
-                for (i = 0; i < categories.length; i++) {
-                    categories[i].disabled=true; 
-                }
+    $('#enable-category').change(function(){
+        if($('#enable-category').is(':checked')){
+            var categories = document.getElementsByClassName("checkbox-category");
+            for (i = 0; i < categories.length; i++) {
+                categories[i].disabled=false ;
             }
-        });
-        
-    </script>
+        }else{
+            var categories = document.getElementsByClassName("checkbox-category");
 
-    <script type="text/javascript">
+            for (i = 0; i < categories.length; i++) {
+                categories[i].disabled=true; 
+            }
+        }
+    });
+        
+    
         $('#myForm').submit(function() {
 
             document.getElementById("graph-panel").style.display = "block";
