92fc22a69cd61421e90d560456d7bf76ab7ff21a
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2015-02-07 02:06:29 +0530

    about us page added

---
+++ about-us.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require 'head.php';?>
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <?php require 'header.php' ?>
+            <?php require 'sidebar.php' ?>
+        </nav>
+
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">About Us</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" id="table-panel-heading">Dimuthu Upeksha</div>
+                            <div class="panel-body">
+                                <img style="max-width:100%;" src="images/upeksha.jpg">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" id="table-panel-heading">Maduranga Siriwardena</div>
+                            <div class="panel-body">
+                                <img style="max-width:100%;" src="images/maduranga.jpg">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="panel panel-default" style="min-height:100%">
+                            <div class="panel-heading" id="table-panel-heading">Chamila Wijayarathna</div>
+                            <div class="panel-body">
+                                <img style="max-width:100%;" src="images/chamila.jpg">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" id="table-panel-heading">Lahiru Lasandun</div>
+                            <div class="panel-body">
+                                <img style="max-width:100%;" src="images/lahiru.jpg">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="js/plugins/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
